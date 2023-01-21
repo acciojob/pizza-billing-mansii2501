2998d5fdb6b6d146937cb5f87ea87475b0c6cbd5
Mansi Garg <mansigarg242@gmail.com>
2023-01-21 14:08:37 +0530

    Pizza Bill

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,10 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+
+        addExtraCheese();
+        addExtraToppings();
+
         // your code goes here
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,8 +6,34 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int toppings;
+
+    private int cheese;
+
+    boolean extraCheese;
+    boolean extraToppings;
+    boolean paperBag;
+    boolean finalBill;
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        this.extraCheese = false;
+        this.extraToppings = false;
+        this.paperBag = false;
+        this.finalBill = false;
+        this.bill = "";
+
+        if(isVeg){
+            this.price = 300;
+            this.toppings = 70;
+        }else{
+            this.price = 400;
+            this.toppings = 120;
+        }
+        this.cheese = 80;
+        this.bill += "Base Price Of The Pizza: "+this.price+"\n";
+
         // your code goes here
     }
 
@@ -17,18 +43,40 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(extraCheese == false){
+            this.price = this.price + cheese;
+            this.extraCheese = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(extraToppings == false){
+            this.price = this.price + toppings;
+            this.extraToppings = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(paperBag == false){
+            this.price = this.price + 20;
+            this.paperBag = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(finalBill == false){
+            if(extraCheese == true)
+                this.bill += "Extra Cheese Added: "+this.cheese+"\n";
+            if(extraToppings == true)
+                this.bill += "Extra Toppings Added: "+this.toppings+"\n";
+            if(paperBag == true)
+                this.bill +="Paperbag Added: "+"20"+"\n";
+            this.bill += "Total Price: "+this.price+"\n";
+            finalBill = true;
+        }
         return this.bill;
     }
 }
